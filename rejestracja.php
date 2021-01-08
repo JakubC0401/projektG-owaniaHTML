00d15a98840760836455cbc873b5f08ed7db8542
Jakub Czyż <jakubczyz990104@gmail.com>
2021-01-08 19:43:51 +0100

    ds

--- rejestracja.php
+++ rejestracja.php
@@ -1,19 +1,40 @@
 <?php
-    require_once("config.php");
-    if(isset($_POST['rejestruj']))
+    
+    $conn = mysqli_connect("localhost", "root", "", 'user');
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    require_once "register.html";
+    //mysql_connect('localhost','root','');
+   //mysql_select_db("user");
+    
+    
+    
+    if (isset($_POST['za']))
     {
-        $login=$_POST['login'];
-        $password=$_POST['pw'];
-
-        $hashPassword = password_hash($password,PASSWORD_BCRYPT);
-
-
-        $add = $db -> prepare('INSERT INTO user (login,password) VALUE (:login,:password)');
-        $sth->bindValue(':login', $login, PDO::PARAM_STR);
-        $sth->bindValue(':password', $hashPassword, PDO::PARAM_STR);
-        $sth->execute();
-
+        $login = $_POST['login'];
+        $haslo1 = $_POST['pw1'];
+        $haslo2 = $_POST['pw2'];
+        $email = $_POST['email'];
+       
         
-        die('Rejestracja pomyslna!');
+        // sprawdzamy czy login nie jest już w bazie
+        if (mysqli_num_rows(mysqli_query("SELECT login FROM user WHERE login = '".$login."';")) == 0)
+        {
+            if ($haslo1 == $haslo2) // sprawdzamy czy hasła takie same
+            {
+                mysqli_query("INSERT INTO `user` (`login`, `password`)
+                    VALUES ('".$login."', '".md5($haslo1)."');");
+                
+                echo "Konto zostało utworzone!";
+            }
+            else echo "Hasła nie są takie same";
+        }
+        else echo "Podany login jest już zajęty.";
+    }
+    else
+    {
+        echo "xD";
     }
 ?>
