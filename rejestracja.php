aa3600a3ab33ab8b4281c881757b2134dad386fb
Blazej <a.blazej8@gmail.com>
2021-01-08 19:57:39 +0100

    dziala polaczenie z baza

--- rejestracja.php
+++ rejestracja.php
@@ -9,22 +9,21 @@
     //mysql_connect('localhost','root','');
    //mysql_select_db("user");
     
-    
+   $login = $_POST['login'];
+   $haslo1 = $_POST['pw1'];
+   $haslo2 = $_POST['pw2'];
+   $email = $_POST['email'];
     
     if (isset($_POST['za']))
-    {
-        $login = $_POST['login'];
-        $haslo1 = $_POST['pw1'];
-        $haslo2 = $_POST['pw2'];
-        $email = $_POST['email'];
-       
-        
+    {    
+        $sql = "SELECT login FROM user WHERE login = '" .$login. "';";
+        $x = mysqli_num_rows(mysqli_query($conn, $sql));
         // sprawdzamy czy login nie jest już w bazie
-        if (mysqli_num_rows(mysqli_query("SELECT login FROM user WHERE login = '".$login."';")) == 0)
+        if ( $x == 0)
         {
             if ($haslo1 == $haslo2) // sprawdzamy czy hasła takie same
             {
-                mysqli_query("INSERT INTO `user` (`login`, `password`)
+                mysqli_query($conn,"INSERT INTO `user` (`login`, `password`)
                     VALUES ('".$login."', '".md5($haslo1)."');");
                 
                 echo "Konto zostało utworzone!";
