cb023b99a684c8cdfe73969756f77b51af2f827d
Jakub Czyż <jakubczyz990104@gmail.com>
2021-01-10 16:04:59 +0100

    rejestracja

--- rejestracja.php
+++ rejestracja.php
@@ -1,23 +1,17 @@
 <?php        
-    $conn = mysqli_connect("userdb1", "1202557_i5E183", "zG1MPHPlyigmJ8", '1202557_i5E183');
+    //$conn = mysqli_connect("userdb1", "1202557_i5E183", "zG1MPHPlyigmJ8", '1202557_i5E183');
+    $conn = mysqli_connect("localhost", "root", "", 'user');
     if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
     }
 
-    function filtruj($zmienna)
-    {
-        if(get_magic_quotes_gpc())
-            $zmienna = stripslashes($zmienna); // usuwamy slashe
-        
-        // usuwamy spacje, tagi html oraz niebezpieczne znaki
-        return mysql_real_escape_string(htmlspecialchars(trim($zmienna)));
-    } 
+    
 
     if (isset($_POST['sumbit']))
     {    
-        $login = filtruj($_POST['login']);
-        $haslo1 = filtruj($_POST['password']);
-        $haslo2 = filtruj($_POST['repeatPassword']);
+        $login = $_POST['login'];
+        $haslo1 = $_POST['password'];
+        $haslo2 = $_POST['repeatPassword'];
 
         $sql = "SELECT login FROM user WHERE login = '" .$login. "';";
         $x = mysqli_num_rows(mysqli_query($conn, $sql));
@@ -27,8 +21,8 @@
             {
                 mysqli_query($conn,"INSERT INTO `user` (`login`, `password`)
                     VALUES ('".$login."', '".md5($haslo1)."');");                
-                echo "<h2>Konto zostało stworzone!</h2>"
-                header("Location: index.php");
+                echo "<h2>Konto zostało stworzone!</h2>";
+                header("Location: index.html");
             }
             else echo "<h2>Hasła nie są takie same!</h2>";
         }
